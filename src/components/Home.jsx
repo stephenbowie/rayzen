eaebcd1893fcbe29294caba9601770b65f23ed8d
zorina <zorinamaika.dev@gmail.com>
2019-04-02 21:05:41 +0800

    simplify route

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 
-
-
 export default class Home extends Component {
   render() {
     return (
-      <div >
+      <div class="home">
+      Home
       </div>
     )
   }
